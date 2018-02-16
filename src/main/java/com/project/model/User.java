791581213764152accdc9d5108f3338554a7f134
kurommnk <e.l.f.eunhyuk@hotmail.com>
2018-02-16 20:27:50 +0700

    [Update] method getData and updateUserLocation in User Class

--- src/main/java/com/project/model/User.java
+++ src/main/java/com/project/model/User.java
@@ -5,6 +5,13 @@
  */
 package com.project.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author KUROMMNK
@@ -53,6 +60,89 @@ public class User {
 
     public void setUserLongtitude(String userLongtitude) {
         this.userLongtitude = userLongtitude;
+    }
+    
+    public String pullUserLocation(boolean allow){
+        return "";
+    }
+    
+    public void updateUserLocation(String userLatitude,String userLongitude){
+        Connection con = null;
+        User user = null;
+        String message = null;
+        try {
+            con = ConnectionBuilder.getConnection();
+            PreparedStatement pstm = con.prepareStatement("UPDATE users set userlatitude = ?, userlongitude = ? WHERE userid = '58130500043';");
+            pstm.setString(1, userLatitude);
+            pstm.setString(2,userLongitude);
+            pstm.executeUpdate();
+            
+//            ResultSet rs = pstm.executeUpdate();
+//            if(rs.next()){
+//                message = rs.getString("message");
+//            }
+//            rs.close();
+            pstm.close();
+            con.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            message = "404 Class not found";
+        } catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            message = "SQL Exception";
+        }
+        
+    }
+    
+    public User getData(){
+        Connection con = null;
+        User user = null;
+        String message = null;
+        try {
+            con = ConnectionBuilder.getConnection();
+            PreparedStatement pstm = con.prepareStatement("select * from users where userid = '58130500043'");                        
+            ResultSet rs = pstm.executeQuery();
+            if(rs.next()){
+                user = new User();
+                user.setId(rs.getString("userid"));
+                user.setUserLatitude(rs.getString("userlatitude"));
+                user.setUserLongtitude(rs.getString("userLongitude"));
+            }
+            rs.close();
+            pstm.close();
+            con.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            message = "404 Class not found";
+        } catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            message = "SQL Exception";
+        }
+        
+        return user;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", userLatitude=" + userLatitude + ", userLongtitude=" + userLongtitude + '}';
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        User u = new User();
+        System.out.println(u.getData());
+        
+        u.updateUserLocation("111.1", "123.1");
+        
+        System.out.println(u.getData());
+        
+        
+//        
+//        System.out.println(u.getData());
+        System.out.println("eiei");
     }
     
 }
